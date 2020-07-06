39e6e59aad59be6367b73ec36eab07b8ef77fa40
João Pedro Santos <potatohunter47@gmail.com>
2020-07-05 21:41:20 -0300

    Create aritmetica.js

---
+++ JavaScriptBasico/aritmetica.js
@@ -0,0 +1,3 @@
+console.log(Math.pow(999999999999999999999999, 999999999999999999999), ', ', Math.round(0.75), ', ', Math.random(), ', ', Math.sqrt(826), ', ', Math.PI)
+console.log(isFinite(Math.pow(999999999999999999999999, 999999999999999999999)))
+console.log(isNaN('Hello World.'))
